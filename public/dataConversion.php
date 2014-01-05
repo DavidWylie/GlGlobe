4f1a944933d0a2e691b82e50d0d67ffa3a769e3c
davidisOrion <david.leo.wylie@googlemail.com>
2014-01-05 22:10:18 +0000

    add earthquakes db and code to convert the earthquake json stuff

---
+++ public/dataConversion.php
@@ -0,0 +1,22 @@
+<?php
+require_once('../src/Db.php');
+
+$data = file_get_contents('../json/all_month.geojson');
+$jsonData = json_decode($data);
+
+$db = new Db('earthquakes.db');
+$db->createTables();
+
+foreach($jsonData as $record) {
+      $recordData = array(
+        'text' => $record->properties->place,
+        'time' => $record->properties->time,
+        'lat'  => $record->geometry->coordinates[0],
+        'lon' => $record->geometry->coordinates[1],
+        'twit_id' => 'na'
+    );
+
+    $db->saveTweet($recordData);
+}
+
+$db->close();
